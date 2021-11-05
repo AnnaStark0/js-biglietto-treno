6d94d0d3ef5acd6c466962911f9afaafbba45ae8
Annastark <89winteriscoming@gmail.com>
2021-11-05 17:13:11 +0100

    calcolo?

--- js/script.js
+++ js/script.js
@@ -1,20 +1,30 @@
-
 //Dati utente
-const km = prompt ('Inserisci quanti km vuoi percorrere');
+const km = prompt('Inserisci quanti km vuoi percorrere');
 
-const eta = prompt ('Inserisci la tua età');
+const eta = prompt('Inserisci la tua età');
 
-//Calcolo prezzo biglietto
+//Prezzo biglietto
 const prezzoBiglietto = km * 0.21;
 
-console.log (prezzoBiglietto)
+console.log(prezzoBiglietto)
 
 //Sconto Minorenni
 const scontoMin = ((prezzoBiglietto * 20) / 100);
 
-console.log (scontoMin);
+console.log(scontoMin);
 
 //Sconto Over 65
 const scontoOver = ((prezzoBiglietto * 40) / 100);
 
-console.log (scontoOver);
+console.log(scontoOver);
+
+//Calcolo prezzo finale
+const bigliettoFinale =
+    if (eta < 18) {
+        (prezzoBiglietto - scontoMin);
+    }
+    else (eta > 18) {
+        (prezzoBiglietto - scontoOver);
+    }
+
+    console.log (bigliettoFinale);
